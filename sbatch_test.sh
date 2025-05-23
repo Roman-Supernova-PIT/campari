005f9cfe1ae7dc051b848a3d9dfcdb122740b20c
Cole Meldorf <cmeldorf@sas.upenn.edu>
2025-05-23 14:07:03 -0400

    added an sbatch file

--- sbatch_test.sh
+++ sbatch_test.sh
@@ -3,14 +3,15 @@
 #SBATCH -p cosmology
 #SBATCH -o multismp_test.out
 #SBATCH -A cosmology
+#SBATCH --mail-user=cmeldorf@sas.upenn.edu
+#SBATCH --mail-type=ALL
 #SBATCH --mem=20G
 
 ## Why doesn't this switch over environments?
 ## Why do we need 20 gigs of memory?
 
-conda init bash
-echo $CONDA_DEFAULT_ENV
+source ~/.bashrc
 conda activate ColeRoman
-echo $CONDA_DEFAULT_ENV
+
 
 python RomanASP.py -s 40120913 -f Y106 -t 2 -d 1
