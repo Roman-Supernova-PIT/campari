fb83f53b15d5ac05749d9357454011ecd6e12c42
Cole Meldorf <cmeldorf@sas.upenn.edu>
2025-06-06 08:52:43 -0400

    added regression message

--- campari/tests/ASP_tests.py
+++ campari/tests/ASP_tests.py
@@ -243,8 +243,8 @@ def test_regression_function(config_path):
 
 
 def test_regression(config_path):
-    # Regression lightcurve was changed on June 4th 2025 because generateGuess
-    # now uses snappl wcs.
+    # Regression lightcurve was changed on June 5th 2025 because generateGuess
+    # had a bug where it divided the guess by the wrong number.
     # Weighting is a Gaussian width 1000 when this was made
     # In the future, this should be True, but random seeds not working rn.
 
