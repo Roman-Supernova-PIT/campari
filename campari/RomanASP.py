bb9eb1df5b5a26d866fab2fc9876d98b1f057822
root <root@localhost>
2025-08-04 14:50:56 +0000

    hp fix

--- campari/RomanASP.py
+++ campari/RomanASP.py
@@ -112,7 +112,7 @@ def main():
                         help="Path to a csv file containing a list of "
                              "OpenUniverse SNIDs to run.")
 
-    parser.add_argument("--healpix", type=int, default=None, required=False,
+    parser.add_argument("--healpix", type=int, default=None, required=False, nargs="*",
                         help="Healpix ID to run on. If given, will run on all "
                              "supernovae in that healpix. nside must be specified with the"
                              "--nside argument.")
