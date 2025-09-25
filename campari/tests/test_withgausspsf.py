f9552c11e971b7600f533e13a79e96eab256a018
Cole Meldorf <colefmeldorf@gmail.com>
2025-09-25 05:58:42 -0700

    Two Gauss Tests

--- campari/tests/test_withgausspsf.py
+++ campari/tests/test_withgausspsf.py
@@ -1,15 +1,12 @@
 import pathlib
-import warnings
 import sys
 
-import galsim
 import numpy as np
-import pytest
-from astropy.utils.exceptions import AstropyWarning
-from erfa import ErfaWarning
+import pandas as pd
+
 
 from campari import RomanASP
-from campari.simulation import simulate_galaxy, simulate_images, simulate_supernova, simulate_wcs
+from campari.simulation import simulate_images
 from snappl.diaobject import DiaObject
 from snappl.image import ManualFITSImage
 from snpit_utils.logger import SNLogger
@@ -84,53 +81,5 @@ def test_simulate_gauss_images():
     np.testing.assert_allclose(images, compare_images.flatten(), atol=1e-7)
 
 
-def test_nogalaxy_no_noise_SNPhotometry(cfg):
-    base_sca = 3
-    base_pointing = 5934
-
-    # curfile = pathlib.Path(pathlib.Path(cfg.value("photometry.campari.paths.debug_dir")) /
-    #                        "deltafcn_test_20172782_Y106_romanpsf_images.npy")
-    # curfile.unlink(missing_ok=True)
-
-    a = ["_", "-s", "20172782", "-f", "Y106", "-n", "0", "-t", "5",
-         "--photometry-campari-psfclass", "gaussian",
-         "--no-photometry-campari-use_real_images",
-         "--no-photometry-campari-fetch_SED",
-         "--photometry-campari-grid_options-type", "none",
-         "--photometry-campari-cutout_size", "19",
-         "--no-photometry-campari-weighting",
-         "--photometry-campari-subtract_background",
-         "--no-photometry-campari-source_phot_ops",
-         "--photometry-campari-simulations-base_sca", str(base_sca),
-         "--photometry-campari-simulations-base_pointing", str(base_pointing),
-         "--photometry-campari-simulations-noise", "0",
-         "--photometry-campari-simulations-run_name", "gauss_test",
-         "--photometry-campari-simulations-bg_gal_flux", "0"]
-    sys.argv = a
-    RomanASP.main()
 
 
-def test_nogalaxy_with_noise_SNPhotometry(cfg):
-    base_sca = 3
-    base_pointing = 5934
-
-    # curfile = pathlib.Path(pathlib.Path(cfg.value("photometry.campari.paths.debug_dir")) /
-    #                        "deltafcn_test_20172782_Y106_romanpsf_images.npy")
-    # curfile.unlink(missing_ok=True)
-
-    a = ["_", "-s", "20172782", "-f", "Y106", "-n", "0", "-t", "5",
-         "--photometry-campari-psfclass", "gaussian",
-         "--no-photometry-campari-use_real_images",
-         "--no-photometry-campari-fetch_SED",
-         "--photometry-campari-grid_options-type", "none",
-         "--photometry-campari-cutout_size", "19",
-         "--no-photometry-campari-weighting",
-         "--photometry-campari-subtract_background",
-         "--no-photometry-campari-source_phot_ops",
-         "--photometry-campari-simulations-base_sca", str(base_sca),
-         "--photometry-campari-simulations-base_pointing", str(base_pointing),
-         "--photometry-campari-simulations-noise", "0",
-         "--photometry-campari-simulations-run_name", "gauss_test",
-         "--photometry-campari-simulations-bg_gal_flux", "0"]
-    sys.argv = a
-    RomanASP.main()
