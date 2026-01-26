70e4bb016d7bc6dad77a3d61ef4415582dabcf9e
root <root@localhost>
2026-01-26 14:54:09 +0000

    im so mad

--- campari/run_one_object.py
+++ campari/run_one_object.py
@@ -169,7 +169,6 @@ def run_one_object(diaobj=None, object_type=None, image_list=None, size=None, ba
         LSB = calculate_local_surface_brightness(cutout_image_list, cutout_pix=2)
 
     # Build the backgrounds loop
-
     model_results = []
     kwarg_dict = {"ra": diaobj.ra, "dec": diaobj.dec, "use_real_images": use_real_images, "grid_type": grid_type,
                   "ra_grid": ra_grid, "dec_grid": dec_grid, "size": size, "pixel": pixel,
