0993dc3db78ac249ca40454b5c54f0f1005a3b82
Cole Meldorf <colefmeldorf@gmail.com>
2026-07-21 15:55:20 +0000

    Still WIP

--- robs_bash_script.sh
+++ robs_bash_script.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
-n_jobs=2
+n_jobs=10
 echo $n_jobs
 #file_len=$(wc -l < /rick/romanisim/STARS_GAIA.csv)
-file_len=644
+file_len=34
 echo $file_len
 stars_per_job=$((file_len / n_jobs ))
 echo $stars_per_job
 
-python -m virtualenv new_venv
+#python -m virtualenv new_venv
 source new_venv/bin/activate
-pip install -e campari/ -e snappl/
+pip install roman_imsim
+pip install -e /home/cfmeldorf/campari/ -e /home/cfmeldorf/snappl/
 pip install healpy
 pip install pytest
-export SNPIT_CONFIG=campari/examples/SMDC/campari_config_test.yaml
+export SNPIT_CONFIG=/home/cfmeldorf/campari/examples/SMDC/campari_config_test.yaml
 
 for ((i=0; i<=file_len - stars_per_job; i+=stars_per_job))
 do
@@ -30,6 +31,6 @@ do
     #         pip install healpy &&
     #         pip install pytest &&
             # export SNPIT_CONFIG=campari/examples/SMDC/campari_config_test.yaml &&
-    nohup python -u asdf_phot_run.py --low_index $i --high_index $upper \
+    nohup python -u /home/cfmeldorf/asdf_phot_run.py --low_index $i --high_index $upper \
     #    "
 done
