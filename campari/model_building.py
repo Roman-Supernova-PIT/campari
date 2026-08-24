3b1f4d08c29c522a02aad1b878b62b0ad3867a46
root <root@localhost>
2026-08-24 20:07:31 +0000

    WIP

--- campari/model_building.py
+++ campari/model_building.py
@@ -287,7 +287,7 @@ def construct_static_scene(ra=None, dec=None, sca_wcs=None, x_loc=None, y_loc=No
     observation_id = image.observation_id if image is not None else None
     sca = image.sca if image is not None else None
 
-    psf_object = PSF.get_psf_object(psfclass, observation_id=observation_id, sca=sca,
+    psf_object = PSF.get_psf_object(psfclass, observation_id=observation_id, sca=sca, size=stampsize,
                                     stamp_size=stampsize, seed=None, image=image)
     # See run_one_object documentation to explain this pixel coordinate conversion.
     x_loc = int(np.floor(x_loc + 0.5))
@@ -363,7 +363,7 @@ def construct_transient_scene(
 
     psf_object = PSF.get_psf_object(
         snpsfclass, observation_id=observation_id, sca=sca,
-        image=image, stamp_size=stampsize, sed=sed
+        image=image, stamp_size=stampsize, sed=sed,size=stampsize,
     )
     psf_image = psf_object.get_stamp(x0=x0, y0=y0, x=x, y=y, flux=flux)
     print_mem("Finished constructing TRANSIENT scene")
